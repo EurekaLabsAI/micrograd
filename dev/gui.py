62ae81c093719ebab6e95f263ba8f85b70742697
Lakshya Agarwal <lakshyajannu@gmail.com>
2024-07-19 09:25:55 -0400

    feat: add computation graph view

--- dev/gui.py
+++ dev/gui.py
@@ -1,15 +1,93 @@
 import ast
 import sys
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
+from graphviz import Digraph
 
 # add current directory to access micrograd module
 sys.path.append(".")
-
 from micrograd import MLP, RNG, Value, nll_loss  # noqa: E402
+
+
+def trace(root):
+    # Traverse the computation graph to extract nodes and edges
+    nodes, edges = set(), set()
+
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+
+    build(root)
+    return nodes, edges
+
+
+def draw_dot(root):
+    # Create a graphviz Digraph object to visualize the computation graph
+    dot = Digraph(format="png", graph_attr={"rankdir": "LR"})
+
+    nodes, edges = trace(root)
+
+    # Create clusters for layers and neurons
+    layer_clusters = {}
+    neuron_clusters = {}
+
+    for n in nodes:
+        layer_name = n.layer_name.split("-")[0]
+        if layer_name not in layer_clusters:
+            layer_clusters[layer_name] = Digraph(name=f"cluster_{layer_name}")
+            layer_clusters[layer_name].attr(
+                label=layer_name, style="filled", color="lightgrey"
+            )
+
+        neuron_name = "-".join(n.layer_name.split("-")[:2])
+        if neuron_name not in neuron_clusters:
+            neuron_clusters[neuron_name] = Digraph(name=f"cluster_{neuron_name}")
+            neuron_clusters[neuron_name].attr(
+                label=neuron_name, style="filled", color="white"
+            )
+
+        # Generate unique identifier for each node
+        uid = str(id(n))
+
+        # Truncate layer name if it is too long
+        layer_name = (
+            f"{n.layer_name[:5]}...{n.layer_name[-5:]}"
+            if len(n.layer_name) > 10
+            else n.layer_name
+        )
+
+        # Add node to appropriate neuron cluster
+        neuron_clusters[neuron_name].node(
+            uid,
+            label=f"{{ {layer_name} | data: {n.data:.4f} | grad: {n.grad:.4f} }}",
+            shape="record",
+        )
+
+        if n._op:
+            # If value is the result of an operation, add a node for it and an edge from the operation to the final value
+            neuron_clusters[neuron_name].node(name=uid + n._op, label=n._op)
+            neuron_clusters[neuron_name].edge(uid + n._op, uid)
+
+    # Add neuron clusters to layer clusters
+    for neuron_name, neuron_cluster in neuron_clusters.items():
+        layer_name = neuron_name.split("-")[0]
+        layer_clusters[layer_name].subgraph(neuron_cluster)
+
+    # Add layer clusters to main graph
+    for layer_cluster in layer_clusters.values():
+        dot.subgraph(layer_cluster)
+
+    # Add edges between nodes
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
+
 
 st.set_page_config(layout="wide", page_title="Micrograd - MLP", page_icon="🧠")
 st.write(
@@ -18,6 +96,8 @@ st.write(
 This application allows you to train a Multi-Layer Perceptron (MLP) to classify points in a 2D plane using an automatic differentiation engine to implement backpropagation.
 
 You can adjust the hyperparameters and visualize the decision boundary of the trained model on the training, validation, and test data splits.
+
+The computation graph of the forward and backward pass of the model on a single data point in the validation set is also visualized to help you understand the flow of data and gradients during training.
 """
 )
 
@@ -128,6 +208,7 @@ random_seed = st.sidebar.number_input(
     "Random seed", min_value=None, max_value=None, value=42
 )
 
+
 # Initialize random number generator
 random = RNG(random_seed)
 
@@ -152,10 +233,11 @@ except (SyntaxError, ValueError):
 
 model = MLP(2, model_layers)
 
-# Plot decision boundary
 if st.button("Train Model"):
+    # Train the model
     model = train_model(model, train_data, n_iters=n_iters)
 
+    # Plot decision boundaries
     with st.spinner("Plotting decision boundaries..."):
         fig_train = plot_decision_boundary(
             model, train_data, h=0.1, title="Decision Boundary [Train Data]"
@@ -177,3 +259,42 @@ if st.button("Train Model"):
 
     with col3:
         st.pyplot(fig_test)
+
+    # Computation graph visualization
+    st.markdown(
+        """## Computation Graph
+                
+The computation graph below shows the forward and backward pass of the model on one random data point from the training set.
+
+Each node represents a value in the computation graph with its data and gradient values, and the edges represent the flow of data during the forward and backward pass."""
+    )
+    with st.spinner("Drawing computation graph..."):
+        random_train_sample = train_data[int(random.uniform(0, len(train_data)))]
+
+        toy_prediction = model(random_train_sample[0])
+        toy_loss = nll_loss(toy_prediction, random_train_sample[1])
+        dot = draw_dot(toy_loss)
+
+    col1, col2 = st.columns([0.2, 0.8])
+
+    with col1:
+        input_data = random_train_sample[0]
+        predicted_logits = toy_prediction
+        predicted_output = np.argmax([logit.data for logit in predicted_logits])
+
+        st.write(f"Input Data: {[round(x, 4) for x in input_data]}")
+        st.write(
+            f"Predicted Logits: {[round(logit.data, 4) for logit in predicted_logits]}"
+        )
+        st.write(f"Predicted Output (Class): {predicted_output}")
+        st.write(f"True Output (Class): {random_train_sample[1]}")
+
+    with col2:
+        st.graphviz_chart(dot, use_container_width=True)
+        st.download_button(
+            label="Download Computation Graph",
+            data=dot.pipe(format="png"),
+            file_name="computation_graph.png",
+            mime="image/png",
+            type="primary",
+        )
--- micrograd.py
+++ micrograd.py
@@ -35,96 +35,124 @@ random = RNG(42)
 # Value
 
 class Value:
-    """ stores a single scalar value and its gradient """
+    """stores a single scalar value and its gradient"""
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op="", layer_name=""):
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
-        self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self._op = _op  # the op that produced this node, for graphviz / debugging / etc
+        self.layer_name = layer_name
 
     def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
+        other = other if isinstance(other, Value) else Value(other, layer_name="const")
+        out = Value(
+            self.data + other.data,
+            (self, other),
+            "+",
+            f"{self.layer_name}+{other.layer_name}",
+        )
 
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
+
         out._backward = _backward
 
         return out
 
     def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, (self, other), '*')
+        other = other if isinstance(other, Value) else Value(other, layer_name="const")
+        out = Value(
+            self.data * other.data,
+            (self, other),
+            "*",
+            f"{self.layer_name}*{other.layer_name}",
+        )
 
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
+
         out._backward = _backward
 
         return out
 
     def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-        out = Value(self.data**other, (self,), f'**{other}')
+        assert isinstance(
+            other, (int, float)
+        ), "only supporting int/float powers for now"
+        out = Value(
+            self.data**other, (self,), f"**{other}", f"{self.layer_name}**{other}"
+        )
 
         def _backward():
-            self.grad += (other * self.data**(other-1)) * out.grad
+            self.grad += (other * self.data ** (other - 1)) * out.grad
+
         out._backward = _backward
 
         return out
 
     def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+        out = Value(
+            0 if self.data < 0 else self.data,
+            (self,),
+            "ReLU",
+            f"ReLU({self.layer_name})",
+        )
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
+
         out._backward = _backward
 
         return out
 
     def tanh(self):
-        out = Value(math.tanh(self.data), (self,), 'tanh')
+        out = Value(math.tanh(self.data), (self,), "tanh", f"tanh({self.layer_name})")
 
         def _backward():
             self.grad += (1 - out.data**2) * out.grad
+
         out._backward = _backward
 
         return out
 
     def exp(self):
-        out = Value(math.exp(self.data), (self,), 'exp')
+        out = Value(math.exp(self.data), (self,), "exp", f"exp({self.layer_name})")
 
         def _backward():
             self.grad += math.exp(self.data) * out.grad
+
         out._backward = _backward
 
         return out
 
     def log(self):
         # (this is the natural log)
-        out = Value(math.log(self.data), (self,), 'log')
+        out = Value(math.log(self.data), (self,), "log", f"log({self.layer_name})")
 
         def _backward():
-            self.grad += (1/self.data) * out.grad
+            self.grad += (1 / self.data) * out.grad
+
         out._backward = _backward
 
         return out
 
     def backward(self):
-
         # topological order all of the children in the graph
         topo = []
         visited = set()
+
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
+
         build_topo(self)
 
         # go one variable at a time and apply the chain rule to get its gradient
@@ -132,35 +160,36 @@ class Value:
         for v in reversed(topo):
             v._backward()
 
-    def __neg__(self): # -self
+    def __neg__(self):  # -self
         return self * -1
 
-    def __radd__(self, other): # other + self
+    def __radd__(self, other):  # other + self
         return self + other
 
-    def __sub__(self, other): # self - other
+    def __sub__(self, other):  # self - other
         return self + (-other)
 
-    def __rsub__(self, other): # other - self
+    def __rsub__(self, other):  # other - self
         return other + (-self)
 
-    def __rmul__(self, other): # other * self
+    def __rmul__(self, other):  # other * self
         return self * other
 
-    def __truediv__(self, other): # self / other
+    def __truediv__(self, other):  # self / other
         return self * other**-1
 
-    def __rtruediv__(self, other): # other / self
+    def __rtruediv__(self, other):  # other / self
         return other * self**-1
 
     def __repr__(self):
-        return f"Value(data={self.data}, grad={self.grad})"
+        return f"Value(data={self.data}, grad={self.grad}, op={self._op}, layer_name={self.layer_name})"
+
 
 # -----------------------------------------------------------------------------
 # Multi-Layer Perceptron (MLP) network
 
-class Module:
 
+class Module:
     def zero_grad(self):
         for p in self.parameters():
             p.grad = 0
@@ -168,16 +197,21 @@ class Module:
     def parameters(self):
         return []
 
-class Neuron(Module):
 
-    def __init__(self, nin, nonlin=True):
+class Neuron(Module):
+    def __init__(self, nin, nonlin=True, layer_name=""):
         r = random.uniform(-1, 1) * nin**-0.5
-        self.w = [Value(r) for _ in range(nin)]
-        self.b = Value(0)
+        self.w = [Value(r, layer_name=f"{layer_name}-W{i}") for i in range(nin)]
+        self.b = Value(0, layer_name=f"{layer_name}-B")
         self.nonlin = nonlin
+        self.layer_name = layer_name
 
     def __call__(self, x):
-        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        # Convert x to Value if it's not already to add layer_name
+        if not all(isinstance(xi, Value) for xi in x):
+            x = [Value(xi, layer_name=f"I{i}") for i, xi in enumerate(x)]
+
+        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
         return act.tanh() if self.nonlin else act
 
     def parameters(self):
@@ -186,10 +220,13 @@ class Neuron(Module):
     def __repr__(self):
         return f"{'TanH' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
 
-class Layer(Module):
 
-    def __init__(self, nin, nout, **kwargs):
-        self.neurons = [Neuron(nin, **kwargs) for _ in range(nout)]
+class Layer(Module):
+    def __init__(self, nin, nout, layer_name="", **kwargs):
+        self.neurons = [
+            Neuron(nin, layer_name=f"{layer_name}-N{i}", **kwargs) for i in range(nout)
+        ]
+        self.layer_name = layer_name
 
     def __call__(self, x):
         out = [n(x) for n in self.neurons]
@@ -201,11 +238,14 @@ class Layer(Module):
     def __repr__(self):
         return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
-class MLP(Module):
 
+class MLP(Module):
     def __init__(self, nin, nouts):
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+        self.layers = [
+            Layer(sz[i], sz[i + 1], f"L{i}", nonlin=i != len(nouts) - 1)
+            for i in range(len(nouts))
+        ]
 
     def __call__(self, x):
         for layer in self.layers:
