89adaa508dc6025a4c5106cfb6ee3f76c7f434c8
JonathanSum <777Jonathansum@gmail.com>
2024-08-23 09:24:02 -0700

    updated the demo js using the canvas

--- src/component/Demo.js
+++ src/component/Demo.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-const Demo = () => {
-    return (
-        <div>Demo</div>
-    )
+const Demo = props => {
+
+    const canvasRef = useRef(null)
+
+    const draw = (ctx, frameCount) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+
+    useEffect(() => {
+
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        let frameCount = 0
+        let animationFrameId
+
+        //Our draw came here
+        const render = () => {
+            frameCount++
+            draw(context, frameCount)
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        render()
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId)
+        }
+    }, [draw])
+
+    return <canvas ref={canvasRef} {...props} />
 }
 
 export default Demo
